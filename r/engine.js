6abe364a03f3ade654a2e45f17e0956c5d6ca8bf
altilunium <70379302+altilunium@users.noreply.github.com>
2020-12-27 19:29:42 +0700

    Add files via upload

--- r/engine.js
+++ r/engine.js
@@ -49,70 +49,16 @@ function wiki2html(s) {
 	
     s = this;
     
-    // lists need to be done using a function to allow for recusive calls
-    function list(str) {
-        return str.replace(/(?:(?:(?:^|\n)[\*#].*)+)/g, function (m) {  // (?=[\*#])
-            var type = m.match(/(^|\n)#/) ? 'OL' : 'UL';
-            // strip first layer of list
-            m = m.replace(/(^|\n)[\*#][ ]{0,1}/g, "$1");
-            m = list(m);
-            return '<' + type + '><li>' + m.replace(/^\n/, '').split(/\n/).join('</li><li>') + '</li></' + type + '>';
-        });
-    }
-    
-    return list(s
-        
-        /* BLOCK ELEMENTS */
-        .replace(/(?:^|\n+)([^# =\*<].+)(?:\n+|$)/gm, function (m, l) {
-            if (l.match(/^\^+$/)) return l;
-            return "\n<p>" + l + "</p>\n";
-        })
 
-        .replace(/(?:^|\n)[ ]{2}(.*)+/g, function (m, l) { // blockquotes
-            if (l.match(/^\s+$/)) return m;
-            return '<blockquote>' + l + '</pre>';
-        })
-        
-        .replace(/((?:^|\n)[ ]+.*)+/g, function (m) { // code
-            if (m.match(/^\s+$/)) return m;
-            return '<pre>' + m.replace(/(^|\n)[ ]+/g, "$1") + '</pre>';
-        })
+    s = s.replace(/\[\[(.*?)\]\]/g, function (m, l) { // internal link or image
+        var p = l.split(/\|/);
+        var link = p.shift();
+        return '<a contenteditable="false" href="' + link + '">' + (p.length ? p.join('|') : link) + '</a>';
+    })
 
-        .replace(/(?:^|\n)([=]+)(.*)\1/g, function (m, l, t) { // headings
-            return '<h' + l.length + '>' + t + '</h' + l.length + '>';
-        })
-    
-        /* INLINE ELEMENTS */
-        .replace(/'''(.*?)'''/g, function (m, l) { // bold
-            return '<strong>' + l + '</strong>';
-        })
-    
-        .replace(/''(.*?)''/g, function (m, l) { // italic
-            return '<em>' + l + '</em>';
-        })
-    
-        .replace(/[^\[](http[^\[\s]*)/g, function (m, l) { // normal link
-            return '<a href="' + l + '">' + l + '</a>';
-        })
-    
-        .replace(/[\[](http.*)[!\]]/g, function (m, l) { // external link
-            var p = l.replace(/[\[\]]/g, '').split(/ /);
-            var link = p.shift();
-            return '<a href="' + link + '">' + (p.length ? p.join(' ') : link) + '</a>';
-        })
-    
-        .replace(/\[\[(.*?)\]\]/g, function (m, l) { // internal link or image
-            var p = l.split(/\|/);
-            var link = p.shift();
+    return s
 
-            if (link.match(/^Image:(.*)/)) {
-                // no support for images - since it looks up the source from the wiki db :-(
-                return m;
-            } else {
-                return '<a contenteditable="false" href="' + link + '">' + (p.length ? p.join('|') : link) + '</a>';
-            }
-        })
-    ); 
+
 }
 
 
@@ -192,43 +138,24 @@ window.onbeforeunload = function(){
 	return null;
 }
 
-
-
+                                                                                                                                
 linkCloserCounter = 0
 
 document.onkeypress = function(e) {
-
-	console.log(e)
-	console.log(e.target)
-	console.log(e.key)
+   
 
 	if (e.key == ']'){
 		linkCloserCounter = linkCloserCounter + 1
 	}
-	else if (e.charCode == 32) {
-		if (linkCloserCounter == 2) {
-			//console.log(e.target.innerHTML)
-			linkCloserCounter = 0
-
-			console.log("kena!!")
-			e.target.innerHTML = e.target.innerHTML.wiki2html()
-        	setEndOfContenteditable(e.target)
-
-
-
-
-
-		}
-	}
-
-	 else {
+	else if ((linkCloserCounter == 2) && (e.charCode == 32)) {
 		linkCloserCounter = 0
+		e.target.innerHTML = e.target.innerHTML.wiki2html()
+    	setEndOfContenteditable(e.target)
 	}
+	else {
+		linkCloserCounter = 0
 
-	console.log(linkCloserCounter)
-
-
-
+	}
 
 	if (getCaretTopPoint().top >= 400) {
 		window.scrollBy(0,100)
